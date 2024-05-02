049497726a68282d1562fce5bec17ada12bdf4af
vikas-kumar-09 <vikashgupta1234x@gmail.com>
2024-05-02 18:39:35 +0530

    some updates

--- src/main/java/in/vikas/chatapp/config/WebSocketEventListener.java
+++ src/main/java/in/vikas/chatapp/config/WebSocketEventListener.java
@@ -1,7 +1,7 @@
 package in.vikas.chatapp.config;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -11,28 +11,31 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import in.vikas.chatapp.model.ChatMessage;
 
 /**
-    * Handles the WebSocket disconnect event.
-    * Sends a leave message to the "/topic/public" destination when a user disconnects.
-    *
-    * param event The SessionDisconnectEvent representing the WebSocket disconnect event.
-    */
+ * Handles the WebSocket disconnect event.
+ * Sends a leave message to the "/topic/public" destination when a user disconnects.
+ *
+ * @param event The SessionDisconnectEvent representing the WebSocket disconnect event.
+ */
 @Component
-@Slf4j
-@RequiredArgsConstructor
 public class WebSocketEventListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
     private final SimpMessageSendingOperations messagingTemplate;
+
+    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if (username != null) {
-            log.info("user disconnected: {}", username);
-            var chatMessage = ChatMessage.builder()
-                    .type(ChatMessage.MessageType.LEAVE)
-                    .sender(username)
-                    .build();
+            logger.info("User disconnected: {}", username);
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
             messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
--- src/main/java/in/vikas/chatapp/model/ChatMessage.java
+++ src/main/java/in/vikas/chatapp/model/ChatMessage.java
@@ -1,79 +1,43 @@
 package in.vikas.chatapp.model;
 
-import lombok.*;
-
-// import java.nio.file.FileStore;
-/**
- * Represents a chat message in the chat application.
- */
-//  i have used the lombok to remove the boilerplate code
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class ChatMessage {
     private String content;
     private String sender;
     private MessageType type;
 
-    /**
-     * Enum representing the type of the chat message.
-     */
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String content, String sender, MessageType type) {
+        this.content = content;
+        this.sender = sender;
+        this.type = type;
+    }
+
     public enum MessageType {
         CHAT, LEAVE, JOIN
     }
 
-    /**
-     * Gets the content of the chat message.
-     *
-     * return the content of the chat message
-     */
     public String getContent() {
         return content;
     }
 
-    /**
-     * Sets the content of the chat message.
-     *
-     * param content the content of the chat message
-     */
     public void setContent(String content) {
         this.content = content;
     }
 
-    /**
-     * Gets the sender of the chat message.
-     *
-     * return the sender of the chat message
-     */
     public String getSender() {
         return sender;
     }
 
-    /**
-     * Sets the sender of the chat message.
-     *
-     * param sender the sender of the chat message
-     */
     public void setSender(String sender) {
         this.sender = sender;
     }
 
-    /**
-     * Gets the type of the chat message.
-     *
-     * return the type of the chat message
-     */
     public MessageType getType() {
         return type;
     }
 
-    /**
-     * Sets the type of the chat message.
-     *
-     * param type the type of the chat message
-     */
     public void setType(MessageType type) {
         this.type = type;
     }
